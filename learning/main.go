534dfab7a93419c58145cb29b48f97c6f1ae3661
m.tajima <masanori.tajima@insightedge.jp>
2024-01-05 11:52:53 +0900

    Add Initial Commit

---
+++ learning/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World!")
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":8000"))
+}
