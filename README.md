7c9766dd1380cc6788eae1f1ef539851b97da642
m.tajima <masanori.tajima@insightedge.jp>
2024-02-12 11:32:49 +0900

    Azure Functionsの追加

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ML OPS with Custom Vision
+
+## デプロイ
+
+### Terraform
+
+以下のコマンドで terraform で Azure 環境を構築します。
+
+```
+cd terraform
+terraform init
+terraform plan
+terraform apply
+```
+
+## ローカル実行
+
+`./src/local.settings.json.sample`を`.sample`を外してコピーし、StorageConnection を埋めてください。
+以下のコマンドでローカル実行します。
+
+```
+cd src
+func start
+```
---
+++ src/function_app.py
@@ -0,0 +1,30 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+
+@app.blob_trigger(
+    arg_name="myblob",
+    path="image-data/{name}",
+    connection="ContainerString",
+)
+def counting_person(myblob: func.InputStream):
+    file_name = myblob.name
+
+    # ファイル名のログ
+    logging.info(f"Name: {myblob.name}")
+    if not check_extension(file_name=file_name):
+        return
+
+
+def check_extension(file_name: str) -> bool:
+    # ファイルの拡張子を確認する
+    file_extension = file_name.split(".")[-1].upper()
+    image_extensions = ["PNG", "JPEG", "BMP"]
+    if file_extension not in image_extensions:
+        logging.error(f"File extension {file_extension} not supported")
+        return False
+    else:
+        logging.info(f"File extension {file_extension} supported")
+        return True
